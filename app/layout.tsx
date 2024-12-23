00ed596fca6766c14c2176845339e81924be1942
Abhishek Sharma <absharma6561@gmail.com>
2024-12-23 12:28:00 -0800

    made dashboard page with side nav and pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/ui/global.css";
+import { inter, lusitana } from "./ui/fonts";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      <p
+        className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+      >
+        <strong> Welcome to Acme.</strong> This is the example for the{" "}
+        <a href="https://nextjs.org/learn/" className="text-blue-500">
+          Next.js Learn Course
+        </a>
+        , brought to you by Vercel.
+      </p>
     </html>
   );
 }
